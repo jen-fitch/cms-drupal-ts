c0ae0eb549d6c15e0330fe429269424ed53129e4
Jen Rees <jf247@humboldt.edu>
2022-03-07 14:03:47 -0800

    updated all files to match https://github.com/chapter-three/next-drupal-basic-starter for a typescript version of cms-drupal example

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,23 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import Link from "next/link"
 
-type Props = {
-  children?: ReactNode
-  title?: string
+import { PreviewAlert } from "@/components/preview-alert"
+
+export function Layout({ children }) {
+  return (
+    <>
+      <PreviewAlert />
+      <div className="max-w-screen-md px-6 mx-auto">
+        <header>
+          <div className="container flex items-center justify-between py-6 mx-auto">
+            <Link href="/" passHref>
+              <a className="text-2xl font-semibold no-underline">
+                Next.js for Drupal
+              </a>
+            </Link>
+          </div>
+        </header>
+        <main className="container py-10 mx-auto">{children}</main>
+      </div>
+    </>
+  )
 }
-
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
-
-export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,60 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Head from "next/head"
+import { GetStaticPropsResult } from "next"
+import { DrupalNode, getResourceCollectionFromContext } from "next-drupal"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+import { NodeArticleTeaser } from "@/components/node-article"
+import { Layout } from "@/components/layout"
 
-export default IndexPage
+interface IndexPageProps {
+  nodes: DrupalNode[]
+}
+
+export default function IndexPage({ nodes }: IndexPageProps) {
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js for Drupal</title>
+        <meta
+          name="description"
+          content="A Next.js site powered by a Drupal backend."
+        />
+      </Head>
+      <div>
+        <h1 className="mb-10 text-6xl font-black">Latest Articles.</h1>
+
+        {nodes?.length ? (
+          nodes.map((node) => (
+            <div key={node.id}>
+              <NodeArticleTeaser node={node} />
+              <hr className="my-20" />
+            </div>
+          ))
+        ) : (
+          <p className="py-4">No nodes found</p>
+        )}
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps(
+  context
+): Promise<GetStaticPropsResult<IndexPageProps>> {
+  const nodes = await getResourceCollectionFromContext<DrupalNode[]>(
+    "node--article",
+    context,
+    {
+      params: {
+        include: "field_image,uid",
+        sort: "-created",
+      },
+    }
+  )
+
+  return {
+    props: {
+      nodes,
+    },
+    revalidate: 10,
+  }
+}
